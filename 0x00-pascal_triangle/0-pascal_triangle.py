771f4ab49a1c6a8ddaeb0f19e8df0fc20eb99066
Ellee007 <alhassanaliyu83@gmail.com>
2024-05-28 07:38:25 -0700

    triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
-"""Contains function that returns a list of lists of integers
-reprsenting the Pascal's triangle
-"""
-
+"""This module contains a function that returns a list of lists of integers representing the Pascal's triangle of n"""
 
 def pascal_triangle(n):
-    """Returns list of lists of pascal's triangle integers
-    """
-
-    list_of_list = []
-    if n > 0:
-        for _ in range(n):
-            pascal_list = []
-
-            # Pascal coeff = nCr = n! / (r! * (n-r)!)
-            # to accommodate index 0
-            n = n - 1
-            for r in range(n + 1):
-                num = factorial(n) / (factorial(r) * factorial(n - r))
-                pascal_list.append(round(num))
-            list_of_list.append(pascal_list)
-
-    return list_of_list[::-1]
-
-
-def factorial(n):
-    """Helper function for calculating factorial"""
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
-    return n * factorial(n-1)
+    """Returns a list of lists of integers representing the Pascal's triangle of n"""
+    # If n is less than or equal to 0, return an empty list
+    if n <= 0:
+        return []
+    # Lists always start with [1] at the top of the triangle
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
