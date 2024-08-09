f0f46f67ab86ce2972a429b7aa47c2c14e573022
Ellee007 <alhassanaliyu83@gmail.com>
2024-08-09 03:35:45 -0700

    file1

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,48 @@
 #!/usr/bin/python3
-""" The prime game
 """
+This is prime game module
+"""
+
+
+def SieveOfEratosthenes(n):
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+
+        # If prime[p] is not
+        # changed, then it is a prime
+        if (prime[p] is True):
+
+            # Update all multiples of p
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+
+    # Print all prime numbers
+    return prime
+
+
 def isWinner(x, nums):
-    """ Dtermines who the winner is
     """
-    maria_wins = 0
-    ben_wins = 0
+    This function check for the winner in this prime game
+    """
+    maria = 0
+    ben = 0
+    for i in range(x):
+        n = nums[i]
+        prime = SieveOfEratosthenes(n)
+        p = 0
+        for j in range(2, n+1):
+            if prime[j]:
+                p += 1
+        if p % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
 
-    for n in nums:
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-
-        # Sieve of Eratosthenes to mark non-prime numbers
-        p = 2
-        while p * p <= n:
-            if primes[p]:
-                for i in range(p * p, n + 1, p):
-                    primes[i] = False
-            p += 1
-
-        # Simulate the game rounds
-        maria_turn = True
-        while True:
-            # Find the next available prime number
-            prime_num = None
-            for i in range(2, n + 1):
-                if primes[i]:
-                    prime_num = i
-                    break
-
-            if prime_num is None:
-                # No prime number left, the current player loses
-                if maria_turn:
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-
-            # Remove the prime number and its multiples
-            for i in range(prime_num, n + 1, prime_num):
-                primes[i] = False
-
-            maria_turn = not maria_turn
-
-    if maria_wins > ben_wins:
+    if maria > ben:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif ben > maria:
         return "Ben"
     else:
         return None
